bbb182f42198b54c11fdd4ca600bb399d5c3d1a3
Paul Kinlan <paul.kinlan@gmail.com>
2019-05-15 21:50:19 +0100

    Updating the build process to make sure it works

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ npm install
 
 sh install-hugo.sh
 
-cp -r node_modules/@editorjs/editorjs/dist static/javascripts/editorjs/
-cp -r node_modules/@editorjs/raw/dist/bundle.js static/javascripts/editorjs/raw.js
-cp -r node_modules/@editorjs/quote/dist/bundle.js static/javascripts/editorjs/quote.js
-cp -r node_modules/@editorjs/header/dist/bundle.js static/javascripts/editorjs/header.js
-cp -r node_modules/@editorjs/paragraph/dist/bundle.js static/javascripts/editorjs/paragraph.js
-cp -r node_modules/@editorjs/code/dist/bundle.js static/javascripts/editorjs/code.js
-cp -r node_modules/@editorjs/list/dist/bundle.js static/javascripts/editorjs/list.js
-cp -r node_modules/@editorjs/link/dist/bundle.js static/javascripts/editorjs/link.js
-cp -r node_modules/@editorjs/simple-image/dist/bundle.js static/javascripts/editorjs/simple-image.js
+cp -r node_modules/@editorjs/editorjs/dist site/static/javascripts/editorjs/
+cp -r node_modules/@editorjs/raw/dist/bundle.js site/static/javascripts/editorjs/raw.js
+cp -r node_modules/@editorjs/quote/dist/bundle.js site/static/javascripts/editorjs/quote.js
+cp -r node_modules/@editorjs/header/dist/bundle.js site/static/javascripts/editorjs/header.js
+cp -r node_modules/@editorjs/paragraph/dist/bundle.js site/static/javascripts/editorjs/paragraph.js
+cp -r node_modules/@editorjs/code/dist/bundle.js site/static/javascripts/editorjs/code.js
+cp -r node_modules/@editorjs/list/dist/bundle.js site/static/javascripts/editorjs/list.js
+cp -r node_modules/@editorjs/link/dist/bundle.js site/static/javascripts/editorjs/link.js
+cp -r node_modules/@editorjs/simple-image/dist/bundle.js site/static/javascripts/editorjs/simple-image.js
 
 ./hugo -d dist --minify
