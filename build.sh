5455b39c6bf78160017ee258e7bf5d7e8c675051
Paul Kinlan <paul.kinlan@gmail.com>
2019-05-17 16:19:33 +0100

    Adding build steps to process files

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 yum install -y wget
+yum install -y ffmpeg
 
 git submodule update --init --recursive -j 8
 
@@ -8,5 +9,8 @@ npm install
 
 sh install-hugo.sh
 
+# Convert all the files.
+# When this gets big we might not want to do this.
+for i in site/static/audio/*.webm; do ffmpeg -i "$i" "site/static/audio/${i%.*}.mp3"; done
 
 ./hugo -s ./site/ -d ../dist --minify
--- site/content/first.md
+++ site/content/first.md
@@ -1,6 +1,8 @@
 ---
 title: "Hello World"
 date: 2018-05-15
+mp3: /audio/first.mp3
+webm: /audio/first.webm
 ---
 
 This is the first post.
