17ef163cdb0e6ac8fa285e2c61b97668edc594e8
Paul Kinlan <paul.kinlan@gmail.com>
2019-05-17 17:32:46 +0100

    Quiet so I can see errors better.

--- install-hugo.sh
+++ install-hugo.sh
@@ -15,13 +15,13 @@ esac
 
 # Install Hugo if not already cached or upgrade an old version.
 if [ ! -e .hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
-  wget --no-check-certificate https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_${MACHINE}-64bit.tar.gz -P .hugo/
+  wget -q --no-check-certificate https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_${MACHINE}-64bit.tar.gz -P .hugo/
   tar xvzf .hugo/hugo_${HUGO_VERSION}_${MACHINE}-64bit.tar.gz -C .hugo/
   cp .hugo/hugo ./hugo
 fi
 
 if [ ! -e ffmpeg ]; then
-  wget --no-check-certificate https://johnvansickle.com/ffmpeg/releases/${FFMPEG_VERSION}
+  wget -q --no-check-certificate https://johnvansickle.com/ffmpeg/releases/${FFMPEG_VERSION}
   FFMPEG="$(tar -tf ${FFMPEG_VERSION} --wildcards '*/ffmpeg')"
   tar -xf ${FFMPEG_VERSION} --wildcards '*/ffmpeg'
   cp ${FFMPEG} .
