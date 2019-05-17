28745779015a5c6894ec8b4bcac8e9460b432081
Paul Kinlan <paul.kinlan@gmail.com>
2019-05-17 16:48:38 +0100

    testing new ffmpeg

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
-
-yum install epel-release -y
-yum update -y
-yum install -y wget ffmpeg
-
-
+yum install -y wget
+rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro && rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+yum install ffmpeg
 git submodule update --init --recursive -j 8
 
 npm install
