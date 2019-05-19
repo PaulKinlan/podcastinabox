d75f8eb2b48a46ad6496073424c4664ee59e2833
Paul Kinlan <paul.kinlan@gmail.com>
2019-05-19 17:33:12 +0100

    Updating the build script a tad.

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 yum install -y wget
-git submodule update --init --recursive -j 8
 
 npm install
 
@@ -10,6 +9,6 @@ sh install-deps.sh
 # When this gets big we might not want to do this.
 # Look for somethign like "size=     329kB time=00:00:21.81 bitrate= 123.4kbits/s speed=46.2x and output to file"
 for i in site/static/audio/*.webm; do ./ffmpeg -y -i "$i" "${i%.*}.mp3"; done
-for i in site/static/audio/*.mp3; do ./ffprobe -i "${i%.*}.mp3" -v quiet -print_format json -show_format -show_streams > "site/data/$(basename $i).json"; done
+for i in site/static/audio/*; do ./ffprobe -i "${i}" -v quiet -print_format json -show_format -show_streams > "site/data/$(basename $i).json"; done
 
 ./hugo -s ./site/ -d ../dist --minify
