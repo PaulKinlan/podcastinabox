a7b77c3f1b368710f5d6f451eb94db94fc5854a6
Paul Kinlan <paul.kinlan@gmail.com>
2019-06-07 19:43:20 +0900

    fixing build

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ sh install-deps.sh
 # When this gets big we might not want to do this.
 # Look for somethign like "size=     329kB time=00:00:21.81 bitrate= 123.4kbits/s speed=46.2x and output to file"
 for i in site/static/audio/*.webm; do ./ffmpeg -y -i "$i" "${i%.*}.mp3"; done
+mkdir site/data
 for i in site/static/audio/*; do ./ffprobe -i "${i}" -v quiet -print_format json -show_format -show_streams > "site/data/$(basename $i).json"; done
 
 ./hugo -s ./site/ -d ../dist --minify
