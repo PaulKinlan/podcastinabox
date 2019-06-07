8f8013eb2a36a525f8a2812f33f34903536f7b91
Paul Kinlan <paul.kinlan@gmail.com>
2019-06-07 19:15:59 +0900

    Add first steps of SW

---
+++ site/static/sw.js
@@ -0,0 +1,38 @@
+ddEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  // Skip the service worker for the feed. The page handles the caching.
+  if (url.origin === location.origin && url.pathname === '/feed') return;
+
+  event.respondWith(async function () {
+
+    // Offline first:
+    const cachedResponse = await caches.match(event.request);
+    return cachedResponse || fetch(event.request);
+  }());
+});
+
+addEventListener('backgroundfetchsuccess', event => {
+  const bgFetch = event.registration;
+
+  event.waitUntil(async function () {
+    const cache = await caches.open(bgFetch.id);
+    const records = await bgFetch.matchAll();
+
+    const promises = records.map(async record => {
+      await cache.put(record.request, await record.responseReady);
+    });
+
+    await Promise.all(promises);
+
+    new BroadcastChannel(bgFetch.id).postMessage({ stored: true });
+  }());
+});
+
+addEventListener('backgroundfetchfail', event => {
+  console.log('Background fetch failed', event);
+});
+
+addEventListener('backgroundfetchclick', event => {
+  clients.openWindow('/');
+});
