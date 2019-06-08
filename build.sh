adebeee013070528e361d1b32b4cb791da76ef92
Paul Kinlan <paul.kinlan@gmail.com>
2019-06-08 11:07:50 +0900

    Only transcode webm to mp3 if it doesn't already exist
    Fix huge detection error

--- build.sh
+++ build.sh
@@ -8,8 +8,14 @@ sh install-deps.sh
 # Convert all the files.
 # When this gets big we might not want to do this.
 # Look for somethign like "size=     329kB time=00:00:21.81 bitrate= 123.4kbits/s speed=46.2x and output to file"
-for i in site/static/audio/*.webm; do ./ffmpeg -y -i "$i" "${i%.*}.mp3"; done
+for i in site/static/audio/*.webm; do 
+  if [ ! -f "${i%.*}.mp3" ]; then
+    ./ffmpeg -y -i "$i" "${i%.*}.mp3"
+  fi
+done
+
 mkdir site/data
+
 for i in site/static/audio/*; do ./ffprobe -i "${i}" -v quiet -print_format json -show_format -show_streams > "site/data/$(basename $i).json"; done
 
 ./hugo -s ./site/ -d ../dist --minify
--- install-deps.sh
+++ install-deps.sh
@@ -14,7 +14,7 @@ case "${unameOut}" in
 esac
 
 # Install Hugo if not already cached or upgrade an old version.
-if [ ! -e .hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
+if [ ! -e .hugo ]; then
   wget -q --no-check-certificate https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_${MACHINE}-64bit.tar.gz -P .hugo/
   tar xvzf .hugo/hugo_${HUGO_VERSION}_${MACHINE}-64bit.tar.gz -C .hugo/
   cp .hugo/hugo ./hugo
