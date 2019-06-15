5d3d3a41758df920335d56919f42c5438751a41f
Paul Kinlan <paul.kinlan@gmail.com>
2019-06-15 20:34:20 +0100

    Correct get

--- site/static/scripts/main.js
+++ site/static/scripts/main.js
@@ -18,7 +18,7 @@ const downloadHandler = async (event) => {
   const image = element.getAttribute('image');
 
   const reg = await navigator.serviceWorker.ready;
-  let bgFetch = await reg.backgroundFetch.getId(id) ||
+  let bgFetch = await reg.backgroundFetch.get(id) ||
                 await reg.backgroundFetch.fetch(id, [src], {
     title: title,
     icons: [{ sizes: '300x300', src: image, type: 'image/jpeg' }],
